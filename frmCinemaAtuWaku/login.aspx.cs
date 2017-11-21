2768d2b58f56dd59698fb448b88da89b1aa5fc70
Alejandro Sanchez O <alejosanosp@hotmail.com>
2017-11-20 19:21:29 -0500

    Logical Login Class

--- frmCinemaAtuWaku/login.aspx.cs
+++ frmCinemaAtuWaku/login.aspx.cs
@@ -1,17 +1,96 @@
-﻿using System;
+﻿using libLogin;
+using System;
 using System.Collections.Generic;
 using System.Linq;
 using System.Web;
 using System.Web.UI;
 using System.Web.UI.WebControls;
 
-namespace AppCinemaAtuWaku
+namespace frmCinemaAtuWaku
 {
-    public partial class Index : System.Web.UI.Page
+    public partial class login : System.Web.UI.Page
     {
+        #region Variables Globales
+        clsLoginRN objLogin;
+        string strNombreAplicacion;
+        #endregion
+
+        #region Metodos Privados
+        private void MostrarMensaje(string PstrMensaje)
+        {
+            this.lblMensaje.Text = PstrMensaje;
+
+            if (PstrMensaje == string.Empty)
+            {
+                this.lblMensaje.Visible = false;
+            }
+            else
+            {
+                this.lblMensaje.Visible = true;
+            }
+        }
+        private bool Validar()
+        {
+            try
+            {
+                if (this.txtUsuario.Text.Trim() == string.Empty && this.txtPassword.Text.Trim() == string.Empty)
+                {
+                    MostrarMensaje("Debe ingresar el documento y la Contraseña para iniciar la sessión");
+                    this.txtUsuario.Focus();
+                    return false;
+                }
+                return true;
+            }
+            catch (Exception ex)
+            {
+                MostrarMensaje(ex.Message);
+                return false;
+            }
+        }
+        private void Ingresar()
+        {
+            try
+            {
+                MostrarMensaje("");
+                if (!Validar())
+                {
+                    return;
+                }
+                objLogin = new clsLoginRN();
+                objLogin.NombreAplicacion = strNombreAplicacion;
+                objLogin.Usuario = this.txtUsuario.Text.Trim();
+                objLogin.Password = this.txtPassword.Text.Trim();
+
+                if (!objLogin.Login())
+                {
+                    MostrarMensaje(objLogin.Error);
+                    objLogin = null;
+                    return;
+                }
+                Session["strTipoUsuario"] = objLogin.Usuario;
+                Response.Redirect("index.aspx", false);
+                objLogin = null;
+            }
+            catch (Exception ex)
+            {
+
+                MostrarMensaje(ex.Message);
+            }
+        }
+        #endregion
+
+        #region Eventos
         protected void Page_Load(object sender, EventArgs e)
         {
-
+            if (IsPostBack)
+            {
+                strNombreAplicacion = System.Reflection.Assembly.GetExecutingAssembly().GetName().Name;
+            }
         }
+        protected void btnLoginButton_Click(object sender, EventArgs e)
+        {
+            Ingresar();
+        }
+        #endregion
     }
 }
